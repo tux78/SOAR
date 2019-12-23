377edbeecca27cb7b1f9c08fdf6c3a393467419e
tux78 <vdberge.sebastian@googlemail.com>
2019-12-23 18:22:33 +0100

    Added shell scripts

--- build.sh
+++ build.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
 # build image
-docker build -t intelmq:PROD -f ./Dockerfile.PROD .
-
-# Move to where the docker-compose resides
-# cd ..
+docker build -t intelmq:PROD -f ./Dockerfile .
 
 # re-create intelmq container
-# docker-compose up --no-start --force-recreate intelmq
+docker-compose up --no-start --force-recreate intelmq
 
 # restart intelmq instance
-# docker-compose restart intelmq
+docker-compose restart intelmq
