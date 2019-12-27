31b231242de7c1203d48fde9fe419db0d1406843
tux78 <vdberge.sebastian@googlemail.com>
2019-12-28 00:03:04 +0100

    Setup Routine

--- setup.sh
+++ setup.sh
@@ -1,36 +1,148 @@
 #!/bin/bash
 
-# create folders
-mkdir misp-db
-mkdir dev_intelmq
+clear_output()
+{
+  tput cup 7 0 && tput ed
+}
 
-# clone intelmq dev
-git clone https://github.com/tux78/intelmq.git ./dev_intelmq
+execute_command()
+{
+  printf "$1"
+  columns=$(expr `tput cols` - ${#1})
 
-# build intelMQ image
-docker build \
-    --rm=true --force-rm=true \
-    -t intelmq:PROD \
-    -f ./Dockerfile.intelmq .
+  printf '%s %s\n' "$(date)" "Running command \'$2\'" >>install.log 
+  echo "Start command output" >>install.log
+  eval $2 &>>install.log
+  echo "End command output" >>install.log
 
-docker build \
-    --rm=true --force-rm=true \
-    -t misp:PROD \
-    -f ./Dockerfile.misp .
+  if [ $? -ne 0 ]
+  then
+    printf "%${columns}s\n" "[FAILED]"
+    return 1
+  fi
+  printf "%${columns}s\n" "[Done]"
+  return 0
+}
 
-#    --build-arg MYSQL_MISP_PASSWORD=ChangeThisDefaultPassworda9564ebc3289b7a14551baf8ad5ec60a \
-#    --build-arg POSTFIX_RELAY_HOST=localhost \
-#    --build-arg MISP_FQDN=localhost \
-#    --build-arg MISP_EMAIL=admin@localhost \
-#    --build-arg MISP_GPG_PASSWORD=ChangeThisDefaultPasswordXuJBao5Q2bps89LWFqWkKgDZwAFpNHvc \
+#########################
+# Start installation
+#########################
 
-# create containers
-docker-compose up --no-start --force-recreate intelmq
-docker-compose up --no-start --force-recreate misp
+# Clear Screen
+clear
+echo "#####################"
+echo
+echo "Welcome to SOAR setup"
+echo
+echo "#####################"
+echo
 
-# start containers
-docker-compose start intelmq
-docker-compose start misp
+# Check prerequisites
+docker &> /dev/null && docker-compose ps &> /dev/null
 
-# Init MISP DB
-docker-compose exec misp /init-db
+if [ $? -ne 0 ]
+then
+  echo "Please install prerequisites"
+  echo "docker and docker-compose are required components"
+  exit 1
+fi
+
+# Set some variables
+# Docker Host IP address
+HOST_IP=$(ping -q -c 1 -t 1 `hostname` | grep PING | sed -e "s/).*//" | sed -e "s/.*(//")
+
+# intelMQ
+read -p "Install intelMQ (y|N): " INTELMQ_INSTALL || INTELMQ_INSTALL = "n"
+
+# Integrate intelMQ with ePO
+if [ "${INTELMQ_INSTALL,,}" == "y" ]
+then
+  read -p "Configure McAfee ePO (y|N): " EPO_CONFIG || EPO_CONFIG="n"
+  if [ "${EPO_CONFIG,,}" == "y" ]
+  then
+    read -p "ePO IP address: " EPO_IP
+    read -e -p "ePO Port [8443]: " -i "8443" EPO_PORT
+    read -p "ePO Admin User: " EPO_ADMIN
+    read -sp "ePO Admin password: " EPO_PW
+    echo
+  fi
+fi
+
+# MISP
+read -p "Install MISP (y|N): " MISP_INSTALL || MISP_INSTALL = "n"
+
+#########################
+# intelMQ Installation
+#########################
+
+if [ "${INTELMQ_INSTALL,,}" == "y" ]
+then
+  # create folders
+  execute_command "Create intelMQ dev directory" "mkdir dev_intelmq"
+
+  # clone intelmq dev
+  execute_command "Clone intelMQ DEV" "git clone https://github.com/tux78/intelmq.git ./dev_intelmq"
+
+  # Build intelMQ image
+  execute_command "Build intelMQ image" "\
+    docker build \
+      -t intelmq:PROD \
+      -f ./intelmq/Dockerfile.intelmq .\
+  "
+
+  # create intelMQ container
+  execute_command "Create intelMQ Container" "\
+    docker-compose up --no-start --force-recreate intelmq\
+  "
+
+  # start intelMQ container
+  execute_command "Start intelMQ" "\
+    docker-compose start intelmq\
+  "
+
+  # Integrate intelMQ with ePO
+  if [ "${EPO_CONFIG,,}" == "y" ]
+  then
+    execute_command "Integrate intelMQ with ePO" "\
+      docker-compose exec intelmq\
+        /usr/local/bin/dxlclient\
+        provisionconfig /etc/intelmq/openDXL $EPO_IP $HOST_IP -t $EPO_PORT -u $EPO_ADMIN -p $EPO_PW\
+    "
+  fi
+fi
+
+#########################
+# MISP Installation
+#########################
+
+if [ "${MISP_INSTALL,,}" == "y" ]
+then
+
+  # create folders
+  execute_command "Create MISP directories" "mkdir misp/misp-db"
+
+  # Build MISP image
+  execute_command "Build MISP image (may take a long time)" "\
+    docker build \
+      --build-arg MISP_FQDN=$HOST_IP \
+      -t misp:PROD \
+    -f ./misp/Dockerfile.misp .\
+  "
+
+  # create MISP container
+  execute_command "Create MISP Container" "\
+    docker-compose up --no-start --force-recreate misp\
+  "
+
+  # start MISP container
+  execute_command "Start MISP" "\
+    docker-compose start misp\
+  "
+
+  # Init MISP DB
+  execute_command "Init MISP DB" "\
+    docker-compose exec misp /init-db\
+  "
+fi
+
+echo "Installation finished!"
